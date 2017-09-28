69dff75dbcc157323f77388d878bbeba463503de
VaishakHolla <VaishakHolla7@gmail.com>
2017-09-28 10:27:54 +0530

    notes

--- MVC/src/model.js
+++ MVC/src/model.js
@@ -29,9 +29,9 @@ class Model {
 module.exports = new Model();
 */
 
+//THIS IS THE SECOND VERSION
 
-
-
+/*
 const Observer = require('./observer');
 
 class Model {
@@ -48,6 +48,61 @@ class Model {
                     this.data.push(value)
                     this.modelAdded.publish(this.data);
                 }
+
+            }
+
+        }
+        xhr.send(null);
+    }
+
+    add(newData) {
+        this.data.push(newData);
+        this.modelAdded.publish(this.data);
+
+    }
+
+    remove(item) {
+        this.data.splice(item, 1);
+        this.modelRemoved.publish(this.data);
+    }
+
+}
+module.exports = new Model();
+*/
+//THIS IS THE THIRD VERSION
+
+//https://pastebin.com/Kbftfb5w
+const Observer = require('./observer');
+
+class Model {
+
+    constructor(data = []) {
+        debugger;
+        this.data = data;
+        this.modelAdded = new Observer();
+        this.modelRemoved = new Observer();
+        let xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            // XHR for Chrome/Firefox/Opera/Safari.
+            xhr.open("Get", "http://localhost:9090", true);
+        } else if (typeof XDomainRequest != "undefined") {
+            // XDomainRequest for IE.
+            xhr = new XDomainRequest();
+            xhr.open("Get", "http://localhost:9090");
+        } else {
+            // CORS not supported.
+            xhr = null;
+        }
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                debugger;
+                for (let value of JSON.parse(xhr.responseText)) {
+                    this.data.push(value)
+                    this.modelAdded.publish(this.data);
+
+                }
+
 
             }
 
--- nodejs/nodeserver.js
+++ nodejs/nodeserver.js
@@ -1,11 +1,43 @@
-const fs = require('fs');
+//THIS IS THE FIRST VERSION
+
+/*const fs = require('fs');
+
 const server = require('http').createServer();
+//instance of event emitter is returned
+server.on('request', (req, res) => {
+    //server.on =event emitter
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(fs.readFileSync('file1'));
+
+});
+server.listen(9090, () => console.log('Server Started'))
+*/
+
+//THIS IS THE SECOND VERSION
+
+//  https://pastebin.com/gZNwH8Rp
+const fs = require('fs');
+
+function pickup(request, response) {
+    //required to process CORS
+    if (request.method == 'OPTIONS') {
+        response.setHeader('Access-Control-Allow-Origin', '*'); //IP can be specified
+        response.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE'); //Methods allowed
+        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type'); //
+        response.end();
+    } else {
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader("Content-Type", "application/json")
+            //response.end();
+    }
+}
+const server = require('http').createServer(pickup); //pickup is needed to define config
+
+
 
 server.on('request', (req, res) => {
-    fs.readFile('file1', (err, data) => {
-        res.write(data);
-    });
-    res.end();
-});
 
-server.listen(9090, () => { console.log("Server started") });
+    res.end(fs.readFileSync('data.json'));
+
+});
+server.listen(9090, () => console.log('Server Started'))
